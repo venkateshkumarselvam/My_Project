2885a61421006034e5cb3cd88ff372ad6a57c44a
Ubuntu <ubuntu@ip-172-31-34-237.ap-south-1.compute.internal>
2024-01-13 08:23:16 +0000

    My Project Commit to local Repository

---
+++ src/main/java/com/assignment/my_assignment/App.java
@@ -0,0 +1,13 @@
+package com.assignment.my_assignment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
